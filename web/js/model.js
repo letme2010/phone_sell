1b923a374c9576f7c7faf2dfbac3486db5389adb
chenzhip <letme2010@gmail.com>
2014-10-06 12:50:16 +0800

    提交列表删了、刷新功能

--- web/js/model.js
+++ web/js/model.js
@@ -48,14 +48,25 @@ function getList(aCallback) {
 
 }
 
-function deleteById(aId) {
+
+function deleteById(aId, aCallback) {
+
 	var db = openDatabase('db1', '1.0', 'db1', 5 * 1024 * 1024);
 	if (db) {
 		db.transaction(function(context) {
-			context.executeSql("delete from t_order where id = " + aId);
+			var sql = "delete from t_order where id = " + aId;
+			context.executeSql(sql,[], function() {
+				
+				if (aCallback) {
+					aCallback();
+				}
+				
+			}, null);
+
 		});
 	}
 }
+
 
 function stringWrap(str) {
 	return "'" + str + "'";
@@ -101,14 +112,17 @@ Date.prototype.Format = function(fmt) {//author: meizz
 }
 
 function onDelectBtnClick(aId) {
-	deleteById(aId);
+	deleteById(aId, function(){
+		console.log('on delete.');
+		updateList();
+	});
 }
 
 function createRow(aRow) {
 	var ret = null;
 	
 	var html = '<tr>\
-					<td>1</td><td><img src="#img_url#" width="60px" height="80px"/></td><td>#name#</td><td>#price#</td><td>#time#</td><td>\
+					<td>#id#</td><td><img src="#img_url#" width="60px" height="80px"/></td><td>#name#</td><td>￥#price#</td><td>#time#</td><td>\
 							<button style="background: gray;color:white;width:50px;height:50px;border: 0" onclick="onDelectBtnClick(this.id);" id="#id#">\
 								X\
 							</button></td>\
@@ -118,9 +132,38 @@ function createRow(aRow) {
 	html = html.replace('#name#', aRow.name);
 	html = html.replace('#time#', aRow.time);
 	html = html.replace('#id#', aRow.id);
+	html = html.replace('#id#', aRow.id);
 	html = html.replace('#price#', aRow.price);
 	console.log(html);
 	ret = $(html);
 	
 	return ret;
 }	
+
+function dropTable() {
+
+	var db = openDatabase('db1', '1.0', 'db1', 5 * 1024 * 1024);
+	if (db) {
+		db.transaction(function(context) {
+			context.executeSql("drop table if exists t_order");
+		});
+	}
+
+}
+
+function updateList() {
+
+	$('#main_list').empty();
+
+	getList(function(aRows) {
+
+			var _jqList = $('#main_list');
+			for (var i = 0, len = aRows.length; i < len; i++) {
+				var row = aRows[i];
+				var _jqTr = createRow(row);
+				_jqList.append(_jqTr);
+			}
+			
+	});
+}
+
